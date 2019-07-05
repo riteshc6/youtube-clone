89bf98194b4fbcaeac7da085916c876c55619f0c
Ritesh Chaurasia <riteshc6@gmail.com>
2019-07-05 10:37:13 +0530

    minor edit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo apt update
 sudo apt install python3-pip
-sudo apt install python3-pip
+sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
